dea6671697691ccf3df4f68107820ce06337e16e
lucaspaiva00 <paival907@gmail.com>
2025-09-19 12:06:44 -0300

    att

---
+++ web/js/scriptcondominio.js
@@ -0,0 +1,26 @@
+const caixaForms = document.querySelector("#caixaForms")
+const uri = "http://localhost:3000/condominiocontroller"
+
+caixaForms.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const data = {
+        nomecondominio: caixaForms.nomecondominio.value,
+        endereco: caixaForms.endereco.value,
+        telefone: caixaForms.telefone.value
+    }
+    fetch(`${uri}`, {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+        .then(res => res.status)
+        .then(status => {
+            if (status == 201) {
+                window.location.reload()
+            }else{
+                alert("Erro ao inserir um Condomínio")
+            }
+        })
+})
